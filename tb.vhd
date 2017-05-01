b4661ed2d44ddc0631834a89dc7e69b72a6eb997
Nick Bukaty <nBukaty@users.noreply.github.com>
2017-04-30 20:51:22 -0500

    added to tb not working yet

--- tb.vhd
+++ tb.vhd
@@ -21,26 +21,40 @@ end tb;
 architecture Behavioral of tb is
     component TrafficLight is
     Port ( rst : in STD_LOGIC;
-   carew : in STD_LOGIC;
-   clk : in STD_LOGIC;
-   CW: in STD_LOGIC;
-   CE: in STD_LOGIC;
-   lights:out STD_LOGIC_VECTOR(5 downto 0));
+               clk : in STD_LOGIC;
+               CW: in STD_LOGIC; --car west
+               CE: in STD_LOGIC; --car east
+               LeftNS: in STD_LOGIC; --car in NS left turn lane
+               lights:out STD_LOGIC_VECTOR(5 downto 0); --NS EW lights
+               leftLight: out STD_LOGIC_VECTOR(2 downto 0)); --NS left turn nights;
    END COMPONENT;
-   signal clk,rst,carew,CW,CE : STD_LOGIC;
+   signal clk,rst,CW,CE,LeftNS : STD_LOGIC;
    signal lights : STD_LOGIC_VECTOR(5 downto 0);
+   signal leftLight STD_LOGIC_VECTOR(2 downto 0);
 begin
-    mapping : Vending_Machine port map (clk => clk, 
-rst => rst, clk => clk, carew=>carew,CW=>CW, CE=>CE, lights=>lights);
-process;
+    mapping : TrafficLight port map (clk => clk, 
+rst => rst, CW=>CW, CE=>CE, LeftNS=>LeftNS,lights=>lights,leftLight=>leftLight);
+process
 begin
-
+rst <='0';
+CW <= '0';
+CE <= '0';
+LeftNS <='0';
+wait for 1 s;
+CW <= '1';
+LeftNS <= '1';
+wait for 4 s;
+CW <= '0';
+wait for 4 s;
+LeftNS <= '0';
+wait for 5 s;
 end process;
 
 process
  begin
-clk <= '1'; wait for 5ns;
-clk <= '0'; wait for 5ns;
+clk <= '1'; wait for 5 ns;
+clk <= '0'; wait for 5 ns;
 end process;
 
 end Behavioral;
+
