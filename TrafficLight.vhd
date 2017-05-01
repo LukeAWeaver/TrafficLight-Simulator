b74b1f34f81b61dc673de6de454a26ea75429bc0
Nick Bukaty <nBukaty@users.noreply.github.com>
2017-04-30 19:36:18 -0500

    add left turn on NS road

--- TrafficLight.vhd
+++ TrafficLight.vhd
@@ -32,11 +32,13 @@ entity TrafficLight is
            clk : in STD_LOGIC;
            CW: in STD_LOGIC;
            CE: in STD_LOGIC;
-           lights:out STD_LOGIC_VECTOR(5 downto 0));
+           LeftNS: in STD_LOGIC;
+           lights:out STD_LOGIC_VECTOR(5 downto 0);
+           leftLight: out STD_LOGIC_VECTOR(2 downto 0));
 end TrafficLight;
 
 architecture Behavioral of TrafficLight is
-type state_type is (GNS, YNS,GEW,YEW);
+type state_type is (GNS, YNS,GEW,YEW, LeftGNS, LeftYNS);
 SIGNAL state: state_type;
 Signal count : integer := 1;
 begin
@@ -56,6 +58,7 @@ case state is
 
 when GNS =>
 lights <="100001";
+leftLight <= "001";
 if(CW = '1' OR CE = '1') then
 count <= 1;
 state <= YNS;
@@ -64,10 +67,9 @@ state <=GNS;
 end if;
 
 
-when YNS => --add delays
---add delays here
+when YNS => 
 lights <="010001";
-if(count > 100000000) THEN
+if(count > 300000000) THEN
 count <= 1;
 state <= GEW;
 else 
@@ -76,6 +78,7 @@ END IF;
 
 when GEW =>
 lights <="001100";
+leftLight <= "001";
 if(CW = '0' AND CE ='0') then
 count <= 1;
 state <=YEW;
@@ -86,13 +89,33 @@ end if;
 when YEW =>
  -- add delay again here
  lights <="001010";
- IF(count >100000000) THEN
+ IF(count >300000000) THEN
  count <=1;
- state <= GNS;
+    IF(LeftNS = '1') THEN
+        state <= LeftGNS;
+    else
+        state <= GNS;
+    end if;
  else 
  state <=YEW;
  END IF;
- 
+     
+ when LeftGNS =>
+    lights <= "001001";
+    leftLight <="100";
+    IF(count >300000000) THEN
+        count <= 1;
+        state <= LeftYNS;
+    else
+        state <= LeftGNS;
+    end if;
+ when LeftYNS =>
+    lights <= "001001";
+    leftLight <= "010";
+    IF(count > 300000000) THEN
+        count <=1;
+        state <= GNS;
+    end if;
 end case;
 
 end if;
