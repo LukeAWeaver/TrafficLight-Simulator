b60a5a77f828213581460113cf6f227193754265
Nick Bukaty <nBukaty@users.noreply.github.com>
2017-05-03 15:40:13 -0500

    changed time scale for simulation
    Had to modify the "count" dependencies in main to get results otherwise it takes too long.

--- tb.vhd
+++ tb.vhd
@@ -12,7 +12,6 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -20,47 +19,42 @@ entity tb is
 end tb;
 architecture Behavioral of tb is
     component TrafficLight is
-    Port (     rst,clk,CW,CE,LeftNS : in STD_LOGIC;
-               lights:out STD_LOGIC_VECTOR(5 downto 0):="000000"; --NS EW lights
-               leftLight: out STD_LOGIC_VECTOR(2 downto 0):="000"); --NS left turn nights;
+    Port ( rst : in STD_LOGIC;
+               clk : in STD_LOGIC;
+               CW: in STD_LOGIC; --car west
+               CE: in STD_LOGIC; --car east
+               LeftNS: in STD_LOGIC; --car in NS left turn lane
+               lights:out STD_LOGIC_VECTOR(5 downto 0); --NS EW lights
+               leftLight: out STD_LOGIC_VECTOR(2 downto 0) ); --NS left turn nights;
    END COMPONENT;
-   signal rst,clk,CW,CE,LeftNS : STD_LOGIC;
+   signal clk,rst,CW,CE,LeftNS : STD_LOGIC;
    signal lights : STD_LOGIC_VECTOR(5 downto 0);
-   signal leftLight: STD_LOGIC_VECTOR(2 downto 0):="000";
+   signal leftLight: STD_LOGIC_VECTOR(2 downto 0);
 begin
-    mapping : TrafficLight port map (rst => rst, 
-    clk => clk,CW=>CW, CE=>CE, LeftNS=>LeftNS,
-    lights=>lights,leftLight=>leftLight);
-    
+    mapping : TrafficLight port map (clk => clk, rst => rst, CW=>CW, CE=>CE, LeftNS=>LeftNS, lights=>lights, leftLight=>leftLight);
+
 process
 begin
-rst<='1';CW <= '0';CE <= '0';LeftNS <='0';
-wait for 20 ns;
 rst <='0';
-wait for 1000000000 ns; --1 second delay
-
-CW <= '1';LeftNS <= '1';
-wait for 1000000000 ns; --4 second delay
-wait for 1000000000 ns;
-wait for 1000000000 ns;
-wait for 1000000000 ns;
-
 CW <= '0';
-wait for 1000000000 ns; -- 1s delay
-
+CE <= '0';
 LeftNS <= '0';
-wait for 1000000000 ns; --5 second delay
-wait for 1000000000 ns;
-wait for 1000000000 ns;
-wait for 1000000000 ns;
-wait for 1000000000 ns;
+wait for 100 ns;
+CW <= '1';
+LeftNS <= '1';
+wait for 1000 ns;
+CW <= '0';
+wait for 400 ns;
+LeftNS <= '0';
+wait for 500 ns;
+CW <= '1';
+wait for 2000 ns;
 end process;
 
 process
  begin
-    clk <= '1'; wait for 5 ns;
-    clk <= '0'; wait for 5 ns;
+clk <= '1'; wait for 5 ns;
+clk <= '0'; wait for 5 ns;
 end process;
 
 end Behavioral;
-
