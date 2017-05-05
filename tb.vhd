34308fce744ae1d37c3034911641b2a411e62ac7
James Rolfe <jamesnrolfe@gmail.com>
2017-05-05 12:02:46 -0500

    use my spacing convention

--- tb.vhd
+++ tb.vhd
@@ -17,44 +17,49 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity tb is
 end tb;
+
 architecture Behavioral of tb is
-    component TrafficLight is
-    Port ( rst : in STD_LOGIC;
-               clk : in STD_LOGIC;
-               CW: in STD_LOGIC; --car west
-               CE: in STD_LOGIC; --car east
-               LeftNS: in STD_LOGIC; --car in NS left turn lane
-               lights:out STD_LOGIC_VECTOR(5 downto 0); --NS EW lights
-               leftLight: out STD_LOGIC_VECTOR(2 downto 0) ); --NS left turn nights;
-   END COMPONENT;
-   signal clk,rst,CW,CE,LeftNS : STD_LOGIC;
-   signal lights : STD_LOGIC_VECTOR(5 downto 0);
-   signal leftLight: STD_LOGIC_VECTOR(2 downto 0);
-begin
-    mapping : TrafficLight port map (clk => clk, rst => rst, CW=>CW, CE=>CE, LeftNS=>LeftNS, lights=>lights, leftLight=>leftLight);
+        component TrafficLight is
+            port(rst: in std_logic;
+                 clk : in std_logic;
+                 CW: in std_logic; --car west
+                 CE: in std_logic; --car east
+                 LeftNS: in std_logic; --car in NS left turn lane
+                 lights:out std_logic_vector(5 downto 0); --NS EW lights
+                 leftLight: out std_logic_vector(2 downto 0)); --NS left turn nights;
+        end component;
+        signal clk,rst,CW,CE,LeftNS: std_logic;
+        signal lights: std_logic_vector(5 downto 0);
+        signal leftLight: std_logic_vector(2 downto 0);
+    begin
+        mapping: TrafficLight port map (clk => clk, 
+                                        rst => rst, 
+                                        CW => CW, 
+                                        CE => CE, 
+                                        LeftNS => LeftNS, 
+                                        lights => lights, 
+                                        leftLight => leftLight);
 
-process
-begin
-rst <='0';
-CW <= '0';
-CE <= '0';
-LeftNS <= '0';
-wait for 100 ns;
-CW <= '1';
-LeftNS <= '1';
-wait for 1000 ns;
-CW <= '0';
-wait for 400 ns;
-LeftNS <= '0';
-wait for 500 ns;
-CW <= '1';
-wait for 2000 ns;
-end process;
+    process
+    begin
+        rst <='0'; CW <= '0'; CE <= '0'; LeftNS <= '0';
+        wait for 100 ns;
+        CW <= '1'; LeftNS <= '1';
+        wait for 1000 ns;
+        CW <= '0';
+        wait for 400 ns;
+        LeftNS <= '0';
+        wait for 500 ns;
+        CW <= '1';
+        wait for 2000 ns;
+    end process;
 
-process
- begin
-clk <= '1'; wait for 5 ns;
-clk <= '0'; wait for 5 ns;
-end process;
+    process
+    begin
+        clk <= '1'; 
+        wait for 5 ns;
+        clk <= '0'; 
+        wait for 5 ns;
+    end process;
 
 end Behavioral;
